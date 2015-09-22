9f6ba68ab6cb76a1b419eb3611af26aad13ff65c
Theresa Cay <cay.theresa@gmail.com>
2015-09-22 15:19:40 -0700

    Added on click function to toggle highlight class for elements with the same classname

--- static/script.js
+++ static/script.js
@@ -7,5 +7,19 @@ if (url) {
 	var source_html = document.getElementById("source_code").innerHTML;
 	source_html = source_html.replace(/&lt;(\/?)(\S+)(.*?)&gt;/g, "<span class=\$2-tag\>&lt;$1$2$3&gt;</span>");
 	document.getElementById("source_code").innerHTML = source_html;
+
+	var previousClassName = null;
+
+	// toggles highlight class for tags with the same class name on click events
+	$('li').click(function(){
+		var className = '.' + this.className;
+
+		if (className !== previousClassName) {
+			$('#source_code').find(previousClassName).removeClass('highlight');
+		}
+
+		$('#source_code').find(className).toggleClass('highlight');
+		previousClassName = className;
+	});
 }
 
