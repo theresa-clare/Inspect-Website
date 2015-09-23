33fe1753b2919138ec5884c2b5807b27d2243d38
Theresa Cay <cay.theresa@gmail.com>
2015-09-23 12:09:44 -0700

    Doctest not representative of output

--- is_valid_url.py
+++ is_valid_url.py
@@ -1,21 +1,6 @@
 import requests
 
 def is_valid_url(url):
-	"""
-	Given a string, returns True if string is a valid URL; returns False otherwise
-	
-	>>> is_valid_url("http://www.facebook.com")
-	True
-
-	>>> is_valid_url("http://facebook.com")
-	True
-
-	>>> is_valid_url("http://thisisnotaurl")
-	False
-
-	>>> is_valid_url("http://www.alsonotavalidurl.com")
-	False
-	"""
 	if url is None:
 		return None, None
 
@@ -27,9 +12,3 @@ def is_valid_url(url):
 		return False, None
 	except requests.exceptions.ConnectionError:
 		return False, None
-
-#################################################################################
-
-if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
