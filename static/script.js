871742c028c6429c7bbff869e7eeef162d8b0212
Theresa Cay <cay.theresa@gmail.com>
2015-09-23 00:06:09 -0700

    Added document.ready function

--- static/script.js
+++ static/script.js
@@ -1,43 +1,44 @@
-if (url) {
-	$('#title-form-msg').removeClass('vertical-center');
-	$('#title-form-msg').addClass('padding-bottom', '10px');
+$(document).ready(function(){
+	if (url) {
+		$('#title-form-msg').removeClass('vertical-center');
+		$('#title-form-msg').addClass('padding-bottom', '10px');
 
-	// loads url into iframe
-	var iframe = document.getElementById("iframe");
-	iframe.src = url;
+		// loads url into iframe
+		var iframe = document.getElementById("iframe");
+		iframe.src = url;
 
-	// wrap spans around each opening and closing tag with class [tagname]-tag
-	var source_html = document.getElementById("source_code").innerHTML;
-	source_html = source_html.replace(/&lt;(\/?)(\w+)(.*?)&gt;/g, "<span class=\$2-tag\>&lt;$1$2$3&gt;</span>");
-	document.getElementById("source_code").innerHTML = source_html;
+		// wrap spans around each opening and closing tag with class [tagname]-tag
+		var source_html = document.getElementById("source_code").innerHTML;
+		source_html = source_html.replace(/&lt;(\/?)(\w+)(.*?)&gt;/g, "<span class=\$2-tag\>&lt;$1$2$3&gt;</span>");
+		document.getElementById("source_code").innerHTML = source_html;
 
-	var previousClassName = null;
+		var previousClassName = null;
 
-	$('li').on({
-		// adds or removes bold text for hover events
-		mouseover: function(){
-			$(this).addClass('hover');
-		},
-		mouseout: function(){
-			$(this).removeClass('hover');
-		},
-		// toggles highlight class for tags with the same class name on click events
-		click: function(){
-			$(this).removeClass('hover');
+		$('li').on({
+			// adds or removes bold text for hover events
+			mouseover: function(){
+				$(this).addClass('hover');
+			},
+			mouseout: function(){
+				$(this).removeClass('hover');
+			},
+			// toggles highlight class for tags with the same class name on click events
+			click: function(){
+				$(this).removeClass('hover');
 
-			var className = '.' + this.className;
+				var className = '.' + this.className;
 
-			if (className !== previousClassName) {
-				$('#source_code').find(previousClassName).removeClass('highlight');
+				if (className !== previousClassName) {
+					$('#source_code').find(previousClassName).removeClass('highlight');
+				}
+
+				$('#source_code').find(className).toggleClass('highlight');
+				previousClassName = className;
+
+				$(this).addClass('hover');
 			}
-
-			$('#source_code').find(className).toggleClass('highlight');
-			previousClassName = className;
-
-			$(this).addClass('hover');
-		}
-	});
-} else {
-	$('#main-wrapper').hide();
-}
-
+		});
+	} else {
+		$('#main-wrapper').hide();
+	}
+});
