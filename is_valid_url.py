eeda5ad3db7c22ee14aef20e4343bbcfbb5926d5
Theresa Cay <cay.theresa@gmail.com>
2015-09-29 15:46:21 -0700

    Refactored to catch multiple exceptions and send appropriate message for respective error

--- is_valid_url.py
+++ is_valid_url.py
@@ -1,14 +1,26 @@
 import requests
 
 def is_valid_url(url):
+	"""
+	Checks if URL is valid. Returns tuple of response html and message.
+	"""
 	if url is None:
-		return None, None
+		return None, "URL is None"
 
 	try:
 		response = requests.get(url)
 		if response.status_code == requests.codes.ok:
 			response_html = response.text
-			return True, response_html
-		return False, None
-	except requests.exceptions.ConnectionError:
-		return False, None
+			return response_html, ""
+		else:
+			return None, "Please check your URL once more"
+	except requests.exceptions.URLRequired: 
+		return None, "A valid URL is required to make a request"
+	except requests.exceptions.ConnectionError: # DNS failures
+		return None, "Please enter a valid URL"
+	except requests.exceptions.HTTPError as e:
+		return None, "An HTTP error occurred: %s" % e.message
+	except requests.exceptions.ConnectTimeout:
+		return None, "The request timed out while trying to connect. Please try again."
+	except:
+		return None, "An unexpected error occurred"
--- server.py
+++ server.py
@@ -1,26 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from is_valid_url import is_valid_url
 from get_tags_and_count import get_tags_and_count
-import re
 
 app = Flask(__name__)
-app.secret_key = "iLoveSlack"
+app.secret_key = "inspect_a_website"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
 	if request.method == 'POST':
 		url = request.form['url']
-		msg = ""
 
 		if not url.startswith("http://") and not url.startswith("https://"):
 			url = "http://" + url
 
-		is_valid, source_code = is_valid_url(url)
+		source_code, msg = is_valid_url(url)
 
-		if not is_valid:
+		if source_code is None:
 			url = ""
-			msg = "Please enter a valid URL"
 			source_code = ""
 
 		tags_and_count = get_tags_and_count(source_code)
